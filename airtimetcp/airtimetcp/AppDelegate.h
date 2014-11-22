ff4da568d26f74d09e9f768f4777aaf336c73e91
Jason Liang <liangjyjason@gmail.com>
2014-11-22 11:00:22 -0500

    init commit

---
+++ airtimetcp/airtimetcp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  airtimetcp
+//
+//  Created by Jason Liang on 11/22/14.
+//  Copyright (c) 2014 Jason Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
